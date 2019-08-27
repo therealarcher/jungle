c74a1b5a44299668d6286bbcb7b488618a3b1cde
Jonathan Archer <jonathan.d.archer@gmail.com>
2019-08-27 03:46:50 +0000

    added user tests.  Haven't yet tested edge cases.

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,74 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  # pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations' do
+    it 'is valid if name, email, and if password and password confirmation match' do
+      @user = User.new
+      @user.name = 'Joe'
+      @user.email = 'joe@gmail.com'
+      @user.password = 'abc123'
+      @user.password_confirmation = 'abc123'
+      expect(@user).to be_valid
+    end
+
+    it 'is not valid if password and password confirmation not equal' do
+      @user = User.new
+      @user.name = 'Joe'
+      @user.email = 'joe@gmail.com'
+      @user.password = 'abc123'
+      @user.password_confirmation = '123abc'
+      expect(@user).to_not be_valid
+    end
+
+    it 'is not valid if email is missing' do
+      @user = User.new
+      @user.name = 'Joe'
+      # @user.email = 'joe@gmail.com'
+      @user.password = 'abc123'
+      @user.password_confirmation = 'abc123'
+      expect(@user).to_not be_valid
+    end
+    
+    it 'is not valid if name is missing' do
+      @user = User.new
+      # @user.name = 'Joe'
+      @user.email = 'joe@gmail.com'
+      @user.password = 'abc123'
+      @user.password_confirmation = 'abc123'
+      expect(@user).to_not be_valid
+    end
+
+    it 'is not valid if password is not minimum length of 5 characters' do
+      @user = User.new
+      @user.name = 'Joe'
+      @user.email = 'joe@gmail.com'
+      @user.password = 'abc'
+      @user.password_confirmation = 'abc'
+      expect(@user).to_not be_valid
+    end
+
+  
+  describe '.authenticate_with_credentials' do
+    it 'should pass with valid credentials' do
+      @user = User.new
+      @user.name = 'Joe'
+      @user.email = 'joe@gmail.com'
+      @user.password = 'abc123'
+      @user.password_confirmation = 'abc123'
+      expect(@user).to be_valid
+    end
+
+    it 'should not pass with invalid credentials' do
+      @user = User.new
+      @user.name = 'Joe'
+      @user.email = 'joe@gmail.com'
+      @user.password = 'abc123'
+      @user.password_confirmation = 'abc124'
+      expect(@user).to_not be_valid
+    end
+  end
+  
+
+  end
+end
